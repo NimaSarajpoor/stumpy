db09a611a43ab3273f0ff668a94680cad7ad86ac
NimaSarajpoor <nimasarajpoor@gmail.com>
2026-02-20 10:01:00 -0500

    minor change

--- stumpy/sdp.py
+++ stumpy/sdp.py
@@ -282,7 +282,7 @@ class _PYFFTW_SLIDING_DOT_PRODUCT:
             rfft_obj.update_arrays(real_arr, complex_arr)
             irfft_obj.update_arrays(complex_arr, real_arr)
 
-        # Compute the circular convolution between T and Q[::-1]
+        # Compute the (circular) convolution between T and Q[::-1]
         # using FFT, and then take the relevant slice of the output
 
         # Step 1
